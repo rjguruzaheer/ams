c55a699a838090bff74f25ecbe5fcb6092c1bae1
rjguruzaheer <69722710+rjguruzaheer@users.noreply.github.com>
2023-03-21 20:34:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".tg").onclick = function () {
+    location.href = "https://telegram.me/Anime_Catalogue"
+};
+
+document.querySelector(".box1").onclick = function () {
+    location.href = "pathan.html"
+};
